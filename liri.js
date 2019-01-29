07c688f2ed57baf5749998ec3fb3d274ff663eba
BG <Bgee269@hotmail.com>
2019-01-29 14:06:16 -0500

    Liri

---
+++ liri.js
@@ -0,0 +1,77 @@
+// Spotify
+
+require("dotenv").config();
+var keys = require("./keys.js");
+var axios = require("axios");
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+
+var axios = require("axios");
+
+var nodeArgs = process.argv;
+
+var songName = "";
+
+for (var i = 2; i < nodeArgs.length; i++) {
+
+  if (i > 2 && i < nodeArgs.length) {
+    songName = songName + "+" + nodeArgs[i];
+  }
+  else {
+    songName += nodeArgs[i];
+
+  }
+}
+
+var queryUrl = "http://www.omdbapi.com/?t=" + songName + "&y=&plot=short&apikey=trilogy";
+
+
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function (response) {
+    console.log("Release Year: " + response.data.Year + "\nTitle: " + response.data.Title + "\nRating: " + response.data.imdbRating + "\nRating: " + response.data.ratingrot + "\nPlot: " + response.data.Plot + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nActors: " + response.data.Actors);
+  }
+);
+
+
+               
+              
+             
+    
+  
+
+// Omdb
+
+// Grab the movieName which will always be the third node argument.
+var axios = require("axios");
+
+var nodeArgs = process.argv;
+
+var movieName = "";
+
+for (var i = 2; i < nodeArgs.length; i++) {
+
+  if (i > 2 && i < nodeArgs.length) {
+    movieName = movieName + "+" + nodeArgs[i];
+  }
+  else {
+    movieName += nodeArgs[i];
+
+  }
+}
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function (response) {
+    console.log("Release Year: " + response.data.Year + "\nTitle: " + response.data.Title + "\nRating: " + response.data.imdbRating + "\nRating: " + response.data.ratingrot + "\nPlot: " + response.data.Plot + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nActors: " + response.data.Actors);
+  }
+);
+
+//
---
+++ liri2.js
@@ -0,0 +1,27 @@
+var axios = require("axios");
+
+var nodeArgs = process.argv;
+
+var artist = "";
+
+for (var i = 2; i < nodeArgs.length; i++) {
+
+  if (i > 2 && i < nodeArgs.length) {
+    artist = artist + "+" + nodeArgs[i];
+  }
+  else {
+    artist += nodeArgs[i];
+
+  }
+}
+
+var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function(response) {
+    console.log("Artist: " + response.data.datetime, "\nVenue: " + response.data.venuename, "\nDate: " + response.data.venuename );
+  }
+);
+//console.log("Date - " + moment(element.datetime).format("MM/DD/YYYY"));
